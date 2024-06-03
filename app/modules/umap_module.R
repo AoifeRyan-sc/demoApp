9fb8c09b438e4471072cea33d884708ddb141eab
AoifeRyan-sc <125266574+AoifeRyan-sc@users.noreply.github.com>
2024-06-03 11:12:00 +0100

    changed density map view to hdbscan view

--- app/modules/umap_module.R
+++ app/modules/umap_module.R
@@ -8,7 +8,8 @@ umapUi <- function(id){
     htmltools::div(
       style = "position: relative",
       shinycssloaders::withSpinner(
-        shiny::uiOutput(ns("display_plot"))
+        # shiny::uiOutput(ns("display_plot"))
+        plotly::plotlyOutput(ns("umap_plot"))
       ),
       htmltools::div(
       style = "position: absolute; top: 10px; left: 10px",
@@ -29,25 +30,35 @@ umapServer <- function(id, r){
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    output$display_plot <- shiny::renderUI({
+    # output$display_plot <- shiny::renderUI({
+    #   if (input$plot_selection == FALSE){
+    #     # print(r$calculating_plot)
+    #     # shiny::req(r$calculating_plot == TRUE, r$input_dataset)
+    #     # print("req satisfied")
+    #     plotly::plotlyOutput(ns("umap_plot"))
+    #   } else {
+    #     # shiny::imageOutput(ns("raster_plot"))
+    #     plotly::plotlyOuptu
+    #   }
+    # })
+    
+    observeEvent(input$plot_selection, {
       if (input$plot_selection == FALSE){
-        # print(r$calculating_plot)
-        # shiny::req(r$calculating_plot == TRUE, r$input_dataset)
-        # print("req satisfied")
-        plotly::plotlyOutput(ns("umap_plot"))
+        r$cluster_type <- "kmeans"
       } else {
-        shiny::imageOutput(ns("raster_plot"))
+        r$cluster_type <- "hdbscan"
+        print("hdbscan")
       }
     })
     
     output$umap_plot <- plotly::renderPlotly({
       if(is.null(r$highlight_df)){
-        createUmap(df = r$df(), highlight_df = NULL, grey_df = NULL)
+        createUmap(df = r$df(), highlight_df = NULL, grey_df = NULL, cluster_type = r$cluster_type)
       } else{
         highlight_points <- r$highlight_df()
         grey_points = r$grey_df()
         
-        createUmap(df = r$df(), highlight_df = highlight_points, grey_df = grey_points)
+        createUmap(df = r$df(), highlight_df = highlight_points, grey_df = grey_points, cluster_type = r$cluster_type)
       }
     })
     
